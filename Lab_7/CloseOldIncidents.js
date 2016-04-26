35b2e92b36a7f055dfb9f5f27ac5dd83e6f48292
daveslusher-sn <dave.slusher@servicenow.com>
2016-04-26 16:10:09 -0400

    Update CloseOldIncidents.js
    Added the limit to keep from closing them all.

--- Lab_7/CloseOldIncidents.js
+++ Lab_7/CloseOldIncidents.js
@@ -3,17 +3,22 @@
 	var gr = new GlideRecord('incident');
 	var body = request.body.data;
 	var return_array = [];
+	var max = 3;
 	
 	var daysAgoParam = body.daysAgo;
-	if (daysAgoParam != null && daysAgoParam != "0") {
+	if (daysAgoParam != null ) {
 		var daysAgo = gs.daysAgo(daysAgoParam);
 		gr.addActiveQuery();
-		gr.addQuery('sys_updated_on', '<', daysAgo);
+		gr.addQuery('sys_updated_on', '<=', daysAgo);
 		gr.query();
-		while(gr.next()){
+		var count = 0;
+		while(gr.next() ){
 			gr.state = 7;
 			gr.update();
-			return_array.push( {"Closed Incident " :gr.number} );
+			return_array.push( {"Closed Incident " :gr.number.toString()} );
+			if (++count  == max) {
+				break;
+			}
 		}
 	}
 	return return_array;
